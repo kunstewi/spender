784359a88e9390854d2d49cd45ab65c667465144
kunstewi <kan.ai35423@gmail.com>
2025-11-15 14:40:42 +0530

    added till auth layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,43 @@
-import Hero from "./components/Hero";
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate
+} from "react-router-dom"
+import Login from "./pages/Auth/Login";
+import SignUp from "./pages/Auth/SignUp";
+import Home from "./pages/Dashboard/Home";
+import Income from "./pages/Dashboard/Income";
+import Expense from "./pages/Dashboard/Expense";
 
 const App = () => {
   return (
     <div>
-      <Hero />
+      <Router>
+        <Routes>
+          <Route path = "/"  element = {<Root />} />
+          <Route path = "/login" exact element = {<Login />} />
+          <Route path = "/signup" exact element = {<SignUp />} />
+          <Route path = "/dashboard" exact element = {<Home />} />
+          <Route path = "/income" exact element = {<Income />} />
+          <Route path = "/expense" exact element = {<Expense />} />
+        </Routes>
+      </Router>
     </div>
-  );
+  )
 };
 
 export default App;
+
+const Root = () => {
+  // to check if authentication token exists in local storage
+  const isAuthenticated = !!localStorage.getItem("token")
+ 
+  // if authenticated return dashboard otherwise login page
+  return isAuthenticated ? (
+    <Navigate to = "/dashboard"/>
+  ) : (
+    <Navigate to = "/login" /> 
+  )
+}
